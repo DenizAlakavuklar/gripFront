a57ab116706dcfd161106c713f03348f279a396b
Lorexia <alexia.lorenza.martinel@protonmail.com>
2023-03-02 14:27:46 +0100

    Adding latest updates on CRUD and routes for trip

--- src/pages/TripPage.jsx
+++ src/pages/TripPage.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
 
-function TripPage() {
-  return (
-    <div>TripPage</div>
+
+import { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+
+const TripPage= () => {
+  const { tripId } = useParams()
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [trip, setTrip] = useState()
+
+  const fetchTrip = async () => {
+    try {
+      const response = await fetch(`http://localhost:5005/trip/trips/${tripId}`)
+      const parsed = await response.json()
+      if (parsed === null) {
+        navigate('/404')
+      } else {
+        console.log(parsed)
+        setTrip(parsed)
+        setIsLoading(false)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTrip()
+  }, [tripId])
+
+  const handleDelete = async () => {
+    await fetch(`http://localhost:5005/trip/trips/${tripId}`, {
+      method: 'DELETE',
+    })
+    navigate('/trips')
+  }
+
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <>
+      <h1>{trip.name}</h1>
+      <p>Description: {trip.description}</p>
+      <Link to={`/trips/update/${trip._id}`}>
+        <button type='button'>Update</button>
+      </Link>
+      <button type='button' onClick={handleDelete}>
+        Delete
+      </button>
+    </>
   )
 }
 
--- src/pages/UpdateTripPage.jsx
+++ src/pages/UpdateTripPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
-function UpdateTripPage() {
-  return (
-    <div>UpdateTripPage</div>
-  )
-}
-
-export default UpdateTripPage
+const UpdateTripPage = () => {
+    const { tripId } = useParams()
+  
+    const [isLoading, setIsLoading] = useState(true)
+    const [trip, setTrip] = useState()
+  
+    const fetchTrip = async () => {
+      try {
+        const response = await fetch(`http://localhost:5005/trip/trips/${tripId}`)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const parsed = await response.json()
+        setTrip(parsed)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    useEffect(() => {
+      fetchTrip()
+    }, [tripId])
+  
+    return (
+      <div>
+        {isLoading ? (
+          <h1>Loading ...</h1>
+        ) : (
+          <TripForm
+            tripName={trip.tripName}
+            image={trip.image}
+            description={trip.description}
+            budget={trip.budget}
+            location={trip.location}
+            attendees={trip.attendees}
+            isUpdating
+            tripId={tripId}
+          />
+        )}
+      </div>
+    )
+  }
+  
+  export default UpdateTripPage
+  

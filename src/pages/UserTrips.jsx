55c9c22d6ba3434aeda6b187f2277bd152f2a031
Krystina Witt <krystinawitt@gmail.com>
2023-03-07 19:09:59 +0100

    show text if trips or attending trips is empty

--- src/pages/UserTrips.jsx
+++ src/pages/UserTrips.jsx
@@ -39,69 +39,67 @@ function UserTrips() {
   
     console.log('These are the user trips', userTrips);
 
-    if (userTrips.length===0){
-        return(<p>You don't have any trips yet!</p>)
-    } 
-    //return (
-        //       <>
-        //         <h1>Explore your next trips:</h1>
-        //         <ul>
-        //             {userTrips.map((userTrip) => {
-        //                 return(
-        //             <li key={userTrip._id}>
-        //                 <div>
-        //                     <Link to={`/trips/${userTrip._id}`}>{userTrip.tripName}</Link>
-        //                 </div>
-        //             </li>
-        //             )})}
-        
-        //         </ul>
-        //       </>
-        //     );
-        //   }
-       
-        return (
-            <>
+    
+    return (
+      <>
 
-            
-            
-            
-            <Container size="xl" px="xs">
-              
+      <Container size="xl" px="xs">
 <h1>Explore your next trips</h1>
-            <Text fz="xl" c="blue.9" fw={700}>Trips you made</Text>
-            <br/>
-      <Grid gutter="lg">
-                     {userTrips.map((userTrip) => {
-                  return(
-           
-  <Grid.Col key={userTrip._id} md={6} lg={3} maw={150}>
- <Card shadow="sm" padding="lg" radius="md" withBorder>
 
- <Text fz="lg" c="blue.9" fw={500}>{userTrip.tripName}</Text>
- <Space h="md" />
-                <Card.Section component="a">
+<Text fz="xl" c="blue.9" fw={700}>Trips you made</Text>
+              <br/>
+        </Container>
+      
+      {userTrips.length > 0 ? 
+      
+      <Container size="xl" px="xs">
+              
+             
+        <Grid gutter="lg">
+                       {userTrips.map((userTrip) => {
+                    return(
+             
+    <Grid.Col key={userTrip._id} md={6} lg={3} maw={150}>
+   <Card shadow="sm" padding="lg" radius="md" withBorder>
+  
+   <Text fz="lg" c="blue.9" fw={500}>{userTrip.tripName}</Text>
+   <Space h="md" />
+                  <Card.Section component="a">
+  
+     
+      <Link to={`/trips/${userTrip._id}/`}>
+                  <img src={userTrip.image} alt="Trip" width="300" /></Link>
+                  </Card.Section>
+                  <p>{userTrip.description}</p>
+                  <Link to={`/trips/${userTrip._id}`}>
+                      <button type='button'>Details</button>
+                  </Link>
+                  </Card>
+                  </Grid.Col>
+                      )})}
+                </Grid>
+  
+              </Container> 
 
-   
-    <Link to={`/trips/${userTrip._id}/`}>
-                <img src={userTrip.image} alt="Trip" width="300" /></Link>
-                </Card.Section>
-                <p>{userTrip.description}</p>
-                <Link to={`/trips/${userTrip._id}`}>
-                    <button type='button'>Details</button>
-                </Link>
-                </Card>
-                </Grid.Col>
-                    )})}
-              </Grid>
-
-            </Container>
+              : 
+              <Container size="xl" px="xs">
+                <p>You have not created any trips yet. <Link to={`/trips/new`}>
+                      Create a trip now!
+                  </Link></p></Container>
+                }
+            
+            
+            
 
 
             <Container size="xl" px="xs" mt={50}>
 
-            <Text fz="xl" c="blue.9" fw={700}>Trips you are attending</Text>
-            <br/>
+            <Text fz="xl" c="blue.9" fw={700}>Other trips that you have been invited to</Text>
+
+            </Container>
+
+            {attendeesTrips.length > 0 ?  
+            <Container size="xl" px="xs" mt={50}>
             <Grid gutter="lg">
            
                      {attendeesTrips.map((userTrip) => {
@@ -131,6 +129,15 @@ function UserTrips() {
            
             </Grid>
             </Container>
+            
+            :
+            
+            <Container size="xl" px="xs" mt={50}>
+            <p>You haven't been invited to other trips. Share Grip with your friends so they can start inviting you.</p>
+              
+       
+              </Container>}
+            
             </>
           )
         }

8d3f7ae8ef7ca4ff77e08553290e1f4d40b341b8
Lorexia <alexia.lorenza.martinel@protonmail.com>
2023-03-08 17:17:48 +0100

    Adding updates to the proposals page

--- src/pages/ProposalDetailsPage.jsx
+++ src/pages/ProposalDetailsPage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useContext } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { SessionContext } from '../contexts/SessionContext';
 import Votes from '../components/Votes';
-import { Card, Image, Flex, Text, Container, Group, Button, Grid, Space } from '@mantine/core';
+import { Card, Image, Flex, Text, Container, Group, Button, Grid, Space, Box, Paper} from '@mantine/core';
 
 
 function ProposalDetailsPage() {
@@ -45,46 +45,62 @@ function ProposalDetailsPage() {
       ) : (
         <>
         <Flex justify="flex-start" align="flex-start" direction={'column'}  >
-        <Link to={`/trips/${proposals.trip}/`}>
-          <button type='button'>Go back to trip</button>
-          </Link>
-       
-            <h1>{proposals.title}</h1>
-            <img src={proposals.image} alt={proposals.title} width="300"/>
-         
-            <p><b>Type:</b> {proposals.type}</p>
-            <p><b>Location:</b> {proposals.location}</p>
-            <p><b>Total Price:</b> {proposals.totalPrice}</p>
-            <p><b>Price per night:</b> {proposals.totalPrice / proposals.nights}</p>
-            <p><b>Nights:</b> {proposals.nights}</p>
-          
-        {/* <Link to={proposals.link} target="_blank">
-            <button type='button'>More info</button>
-        </Link> */}
+           <Box mt={40} ml={50}>
+              <Link to={`/trips/${proposals.trip}/`} >
+                <button type='button'>Go back to trip</button>
+              </Link>
+           </Box>
+        </Flex>
 
-        {proposals.link2 ? (<Link to={proposals.link2} target="_blank">
-            <button type='button'>More info</button>
-        </Link>) : '' }
+            <Flex justify="center" align="center" direction={'column'}>
+                  <h1>{proposals.title}</h1>
+                  <Image src={proposals.image} alt="Trip" width={800} height={400} radius="md"/>
 
-        <p><b>Votes:</b></p>
+          <Flex>
+            <Box>
+              <Paper mt={25} mr={50} ml={50} shadow="xl" radius="md" p={100} width={900}>
+                  <p><b>Type:</b> {proposals.type}</p>
+                  <p><b>Location:</b> {proposals.location}</p>
+                  <p><b>Total Price:</b> {proposals.totalPrice}</p>
+                  <p><b>Price per night:</b> {proposals.totalPrice / proposals.nights}</p>
+                  <p><b>Nights:</b> {proposals.nights}</p>
+                
+              {/* <Link to={proposals.link} target="_blank">
+                  <button type='button'>More info</button>
+              </Link> */}
 
-<Votes proposal={proposals} allVotes={proposals.votes} trip={tripId} tripId={tripId}/>
-<p>Added By: {proposals.createdBy.username} <img src={proposals.createdBy.picture} width="20"/></p>
+              {proposals.link2 ? (<Link to={proposals.link2} target="_blank">
+                  <button type='button'>More info</button>
+              </Link>) : '' }
 
-        {/* Only show update and delete buttons if you were the creator */}
-        {userId===proposals.createdBy._id ? 
-        <>
-          {/* <Link to={`/${proposals.trip}/${proposals._id}/update/`}>
-          <button type='button'>Update</button>
-          </Link> */}
-          <button type='button' onClick={(e)=>{handleProposalDelete(proposals._id)}}>
-              Delete
-          </button>
-        </>
-        : ""}
-   
+              </Paper>
+            </Box>
 
+
+          <Box>
+            <Paper mt={25} mr={50} ml={50} shadow="xl" radius="md" p={100} width={900}>
+                  <p><b>Votes:</b></p>
+
+                    <Votes proposal={proposals} allVotes={proposals.votes} trip={tripId} tripId={tripId}/>
+                    <p>Added By: {proposals.createdBy.username} <img src={proposals.createdBy.picture} width="20"/></p>
+
+                  {/* Only show update and delete buttons if you were the creator */}
+                  {userId===proposals.createdBy._id ? 
+                  <>
+                    {/* <Link to={`/${proposals.trip}/${proposals._id}/update/`}>
+                    <button type='button'>Update</button>
+                    </Link> */}
+                    <button type='button' onClick={(e)=>{handleProposalDelete(proposals._id)}}>
+                        Delete
+                    </button>
+                  </>
+                  : ""}
+              </Paper>
+            </Box>
       </Flex>
+        
+        </Flex>
+      
         </>
       )
 }
